4313407c4d3c21c92ce39245652a0f666dfc0232
ymoris52 <ymoris52@gmail.com>
2025-03-16 18:23:49 +0900

    fix: missing consideration for question version

--- course_master_questions.sql
+++ course_master_questions.sql
@@ -1,43 +1,34 @@
-SELECT
-	id,
-	parent,
-	name,
-	questiontext,
-	generalfeedback,
-	qtype
+SELECT DISTINCT
+	question.id,
+	question.parent,
+	question.name,
+	question.questiontext,
+	question.generalfeedback,
+	question.qtype
 FROM
-	{{ db_prefix }}question
+	{{ db_prefix }}question question,
+	{{ db_prefix }}question_versions question_versions,
+	{{ db_prefix }}question_references question_references,
+	{{ db_prefix }}quiz_slots quiz_slots
 WHERE
-	id IN (
+	question.id = question_versions.questionid
+	AND question_versions.questionbankentryid = question_references.questionbankentryid
+	AND question_references.component = 'mod_quiz'
+	AND question_references.questionarea = 'slot'
+	AND question_references.itemid = quiz_slots.id
+	AND quiz_slots.quizid IN (
 		SELECT
-			question.id
+			quiz.id
 		FROM
-			{{ db_prefix }}question question,
-			{{ db_prefix }}question_versions question_versions,
-			{{ db_prefix }}question_bank_entries question_bank_entries,
-			{{ db_prefix }}question_references question_references,
-			{{ db_prefix }}quiz_slots quiz_slots
+			{{ db_prefix }}course_modules course_modules,
+			{{ db_prefix }}context context,
+			{{ db_prefix }}quiz quiz,
+			{{ db_prefix }}modules modules
 		WHERE
-			question.id = question_versions.questionid
-			AND question_versions.questionbankentryid = question_bank_entries.id
-			AND question_bank_entries.id = question_references.questionbankentryid
-			AND question_references.component = 'mod_quiz'
-			AND question_references.questionarea = 'slot'
-			AND question_references.itemid = quiz_slots.id
-			AND quiz_slots.quizid IN (
-				SELECT
-					quiz.id
-				FROM
-					{{ db_prefix }}course_modules course_modules,
-					{{ db_prefix }}context context,
-					{{ db_prefix }}quiz quiz,
-					{{ db_prefix }}modules modules
-				WHERE
-					course_modules.course = %s
-					AND course_modules.id = context.instanceid
-					AND course_modules.instance = quiz.id
-					AND context.contextlevel = 70
-					AND course_modules.module = modules.id
-					AND modules.name = 'quiz'
-			)
+			course_modules.course = %s
+			AND course_modules.id = context.instanceid
+			AND course_modules.instance = quiz.id
+			AND context.contextlevel = 70
+			AND course_modules.module = modules.id
+			AND modules.name = 'quiz'
 	)
--- course_master_quiz_slots.sql
+++ course_master_quiz_slots.sql
@@ -1,13 +1,45 @@
+WITH
+	question_versions_max AS (
+		SELECT
+			questionbankentryid,
+			MAX(version) AS version
+		FROM
+			{{ db_prefix }}question_versions
+		WHERE
+			status = 'ready'
+		GROUP BY
+			questionbankentryid
+	),
+	question_references_2 AS (
+		SELECT
+			question_references.itemid,
+			question_references.questionbankentryid,
+			CASE
+				WHEN question_references.version IS NULL THEN question_versions_max.version
+				ELSE question_references.version
+			END AS version
+		FROM
+			{{ db_prefix }}question_references question_references,
+			question_versions_max
+		WHERE
+			question_references.questionbankentryid = question_versions_max.questionbankentryid
+			AND question_references.component = 'mod_quiz'
+			AND question_references.questionarea = 'slot'
+	)
 SELECT
-	id,
-	slot,
-	quizid,
-	page,
-	maxmark
+	quiz_slots.id,
+	quiz_slots.slot,
+	quiz_slots.quizid,
+	quiz_slots.page,
+	quiz_slots.maxmark,
+	question.id
 FROM
-	{{ db_prefix }}quiz_slots
+	{{ db_prefix }}quiz_slots quiz_slots,
+	question_references_2,
+	{{ db_prefix }}question_versions question_versions,
+	{{ db_prefix }}question question
 WHERE
-	quizid IN (
+	quiz_slots.quizid IN (
 		SELECT
 			quiz.id
 		FROM
@@ -23,6 +55,10 @@ WHERE
 			AND course_modules.module = modules.id
 			AND modules.name = 'quiz'
 	)
+	AND quiz_slots.id = question_references_2.itemid
+	AND question_references_2.questionbankentryid = question_versions.questionbankentryid
+	AND question_references_2.version = question_versions.version
+	AND question_versions.questionid = question.id
 ORDER BY
-	quizid,
-	slot
+	quiz_slots.quizid,
+	quiz_slots.slot
